fc855cc166e604e84e852140a12555872914d1b9
Ricky <rmedinan2009@gmail.com>
2025-10-04 15:49:10 -0500

    ajustes config.py

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
-# Configuración general del proyecto
+import os
 
-API_URL = "http://localhost:3000/api"   # URL backend Express
-SECRET_KEY = "clave_super_secreta"      # 🔑 cámbiala en producción
+# ===============================
+# CONFIGURACIÓN GENERAL DEL PROYECTO
+# ===============================
+
+# Si existe una variable de entorno API_URL (Render), la usa.
+# Si no, usa el backend local (para desarrollo).
+API_URL = os.getenv("API_URL", "http://localhost:3000/api")
+
+SECRET_KEY = os.getenv("SECRET_KEY", "clave_super_secreta")
